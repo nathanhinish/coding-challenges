1309ebab41dccde4e1f4c0c7b67fb684b2d0afc7
Nathan Hinish <nathan.hinish@mheducation.com>
2025-12-17 10:27:19 -0800

    fix typo in footer

--- react-handmade-childrens-books/app/blocks/site-footer.tsx
+++ react-handmade-childrens-books/app/blocks/site-footer.tsx
@@ -2,7 +2,7 @@ export function SiteFooter() {
   return (
     <footer className="rounded mt-4">
       <div className="row fs-6">
-        <div className="col">&copy; 2025 HBC, LLC.</div>
+        <div className="col">&copy; 2025 HCB, LLC.</div>
         <div className="col">
           <a href="tel:1234567890">123-456-7890</a>
         </div>
